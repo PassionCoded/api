d1fb75db94a17694a91f478b2eaa37286449dc5d
Matt Yang <yang70@gmail.com>
2016-06-16 21:06:29 -0700

    able to register user with post to /reg_user

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,25 @@
+class RegistrationController < ApplicationController
+  def register_user
+    @user = User.create(user_params)
+
+    if @user.save
+      render json: payload(@user)
+    else
+      render json: { errors: @user.errors.full_messages }, status: 422
+    end
+  end
+
+  private
+
+  def payload(user)
+    return nil unless user and user.id
+    {
+      auth_token: JsonWebToken.encode({ user_id: user.id }),
+      user: { id: user.id, email: user.email }
+    }
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post 'auth_user' => 'authentication#authenticate_user'
+  post 'reg_user' => 'registration#register_user'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
