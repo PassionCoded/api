51a9c3eeb57866f32aa358e4d5a5a65831b51a4c
Matt Yang <yang70@gmail.com>
2016-07-06 14:57:44 -0700

    passion creation is now verifying first then creating, all routes returning correct object

--- app/controllers/passions_controller.rb
+++ app/controllers/passions_controller.rb
@@ -4,11 +4,14 @@ class PassionsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
 
-    @profile = Profile.find_by(user: @user)
+    @passions = passion_params[:passions]
 
-    @passions = process_passions(passion_params[:passions])
-    
-    render json: payload(@user, @profile, @passions)
+    if verify_passions
+      process_passions
+      render json: payload(@user)
+    else
+      render json: { errors: ['Passions data formatted incorrectly'] }, status: 400
+    end
   end
 
   private
@@ -17,53 +20,24 @@ class PassionsController < ApplicationController
     params.permit(:passions => [:name])
   end
 
-  def process_passions(passions_array)
-    saved_passions = []
-
-    if passions_array.class != Array
-      render json: { errors: ['Passions data must be an array'] }, status: 400
+  def verify_passions
+    if @passions.class != Array
+      false
     else
-      passions_array.each do |passion|
-        @passion = Passion.new(name: passion[:name], user: current_user)
-
-        if @passion.save
-          saved_passions.push(@passion)
-        else
-          render json: { errors: @passion.errors.full_messages }, status: 422
-        end
+      @passions.each do |p|
+        return false unless p.keys.include? "name"
+        return false unless p["name"].class == String
       end
     end
 
-    format_passions(saved_passions)
+    true
   end
 
-  def format_passions(input)
-    formatted = []
+  def process_passions
+    @passions.each do |passion|
+      passion = Passion.new(name: passion[:name], user: @user)
 
-    input.each do |p|
-      formatted.push({
-        name: p.name
-      })
+      passion.save
     end
-
-    formatted
-  end
-
-  def payload(user, profile, passions=[])
-    {
-      user: {
-        id: user.id,
-        email: user.email,
-        profile: {
-          first_name: profile.first_name,
-          last_name: profile.last_name,
-          profession: profile.profession,
-          tech_of_choice: profile.tech_of_choice,
-          years_experience: profile.years_experience,
-          willing_to_manage: profile.willing_to_manage,
-        },
-        passions: passions
-      }
-    }
   end
 end
