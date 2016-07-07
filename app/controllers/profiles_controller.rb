1080b75c768b97f41414d415394cd9d0d219919d
Matt Yang <yang70@gmail.com>
2016-07-06 22:04:56 -0700

    update error handling and field verification for profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,14 +4,18 @@ class ProfilesController < ApplicationController
   def create
     @user = User.find(current_user.id)
 
-    @profile = Profile.new(profile_params)
-    @profile.user = @user
-    authorize! :create, @profile
+    if verify_profile_params
+      @profile = Profile.new(profile_params)
+      @profile.user = @user
+      authorize! :create, @profile
 
-    if @profile.save
-      render json: payload(@user, @profile)
+      if @profile.save
+        render json: payload(@user)
+      else
+        render json: { errors: @profile.errors.full_messages }, status: 422
+      end
     else
-      render json: { errors: @profile.errors.full_messages }, status: 422
+      render json: { errors: ['One or more profile fields is missing or empty'] }, status: 400
     end
   end
 
@@ -21,21 +25,13 @@ class ProfilesController < ApplicationController
     params.require(:profile).permit(:first_name, :last_name, :profession, :tech_of_choice, :years_experience, :willing_to_manage)
   end
 
-  def payload(user, profile, passions=[])
-    {
-      user: {
-        id: user.id,
-        email: user.email,
-        profile: {
-          first_name: profile.first_name,
-          last_name: profile.last_name,
-          profession: profile.profession,
-          tech_of_choice: profile.tech_of_choice,
-          years_experience: profile.years_experience,
-          willing_to_manage: profile.willing_to_manage,
-        },
-        passions: passions
-      }
-    }
+  def verify_profile_params
+    required_fields = [:first_name, :last_name, :profession, :tech_of_choice, :years_experience, :willing_to_manage]
+
+    required_fields.each do |field|
+      return false if profile_params[field].nil? || profile_params[field] == ""
+    end
+
+    true
   end
 end
