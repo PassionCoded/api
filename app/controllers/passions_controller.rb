9cf44905422f200f1df6f4d4fd6d4ba3adf81e02
Matt Yang <yang70@gmail.com>
2016-08-07 10:05:57 -0700

    error when dupe passion, better error messages

--- app/controllers/passions_controller.rb
+++ app/controllers/passions_controller.rb
@@ -2,7 +2,13 @@ class PassionsController < ApplicationController
   before_filter :authenticate_request!
 
   def create
-    @user = User.find(current_user.id)
+    @user   = User.find(current_user.id)
+    @error = nil
+    @error_messages = {
+      1 => { errors: ['Passions data formatted incorrectly'] },
+      2 => { errors: ['Passions data cannot be blank'] },
+      3 => { errors: ['Passions data already exists'] },
+    }
 
     if params[:passions].class != Array
       render json: { errors: ['Passions must be an array of passion objects'] }, status: 400
@@ -12,7 +18,7 @@ class PassionsController < ApplicationController
         process_passions
         render json: payload(@user)
       else
-        render json: { errors: ['Passions data formatted incorrectly or is blank'] }, status: 400
+        render json: @error_messages[@error], status: 400
       end
     end
   end
@@ -24,13 +30,22 @@ class PassionsController < ApplicationController
   end
 
   def verify_passions
-    if @passions.class != Array
-      false
-    else
-      @passions.each do |p|
-        return false unless p.keys.include? "name"
-        return false unless p["name"].class == String
-        return false if p["name"] == ""
+    current_passions_names = []
+
+    Passion.where(user: current_user).each do |p|
+      current_passions_names.push p.name
+    end
+
+    @passions.each do |p|
+      if !p.keys.include? "name" || !p["name"].is_a?(String) || !!p["name"] == p["name"]
+        @error = 1
+        return false
+      elsif p["name"] == ""
+        @error = 2 
+        return false 
+      elsif current_passions_names.include? p[:name]
+        @error = 3
+        return false
       end
     end
 
@@ -39,14 +54,13 @@ class PassionsController < ApplicationController
 
   def process_passions
     @passions.each do |passion|
-      passion = Passion.new(name: passion[:name].downcase, user: @user)
-      current_passions_names = []
+      passion[:name] = passion[:name].downcase if passion[:name].class == String
+    end
 
-      Passion.where(user: current_user).each do |p|
-        current_passions_names.push p.name
-      end
+    @passions.uniq!
 
-      next if current_passions_names.include? passion[:name]
+    @passions.each do |passion|
+      passion = Passion.new(name: passion[:name], user: @user)
 
       passion.save
     end
