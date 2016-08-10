2f1a80ce0c2713f5eadb6d741cc9e0629f6e6bd0
Matt Yang <yang70@gmail.com>
2016-08-10 09:04:03 -0700

    update tests for new passion delete procedure

--- spec/requests/passions/passion_delete_spec.rb
+++ spec/requests/passions/passion_delete_spec.rb
@@ -18,38 +18,24 @@ RSpec.describe "passion api", :type => :request do
     end
 
     it "deletes correct passion with DELETE request to /passions" do
-      passion_to_delete = {
-        "passions": [
-          { "name": "one" }
-        ]
-      }
+      passion_to_delete = Passion.where(user: User.first)[0]
 
-      delete "/passions", passion_to_delete, {'Authorization': user_token(User.first)}
+      delete "/passions/#{passion_to_delete.id}", nil, {'Authorization': user_token(User.first)}
 
       expect(response_as_json[:user][:passions][0][:name]).to eq("two")
     end
 
-    it "deletes multiple passions with DELETE request to /passions" do
-      passions_to_delete = {
-        "passions": [
-          { "name": "one" },
-          { "name": "two" }
-        ]
-      }
-
-      delete "/passions", passions_to_delete, {'Authorization': user_token(User.first)}
-
-      expect(response_as_json[:user][:passions][0][:name]).to eq("three")
-    end
-
     it "deletes nothing with DELETE request to /passions with passion not in the user's passion" do
-      passion_to_delete = {
-        "passions": [
-          { "name": "fake" }
-        ]
-      }
+      all_passions = Passion.where(user: User.first)
+      passion_ids = []
 
-      delete "/passions", passion_to_delete, {'Authorization': user_token(User.first)}
+      all_passions.each do |p|
+        passion_ids.push p.id
+      end
+
+      nonexistant_passion = passion_ids.inject(:+)
+
+      delete "/passions/#{nonexistant_passion}", nil, {'Authorization': user_token(User.first)}
 
       expect(response_as_json[:user][:passions][0][:name]).to eq("one")
       expect(response_as_json[:user][:passions][1][:name]).to eq("two")
